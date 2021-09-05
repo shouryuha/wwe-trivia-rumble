4a6ae7b9bd05ec89977ff1057da17ce54af1e50b
shouryuha <edu-nakamura@hotmail.com>
2021-09-05 09:07:12 -0300

    nuild

--- src/App.js
+++ src/App.js
@@ -1,12 +1,8 @@
-import logo from './logo.svg';
 import './App.css';
-import Button from '@material-ui/core/Button';
 import TriviaList from './triviaList'
 
 function App() {
-  const sayHello = () => {
-    console.log('Gello')
-  }
+ 
   return (
     <div>
       <TriviaList />
